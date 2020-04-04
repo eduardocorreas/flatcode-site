434a943094f48357d46240e3ca0429b265fafa2f
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2020-04-04 18:46:28 -0300

    initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,42 +5,25 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import './layout.css'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+    <div className="bg-gray-800">
+      <Header />
+      <div className="h-auto">
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
-    </>
+      <footer className="text-white p-3 text-center">
+        © {new Date().getFullYear()}
+        {` `}
+        <a href="https://www.flatcodesm.com">Flatcode</a>
+      </footer>
+    </div>
   )
 }
 
