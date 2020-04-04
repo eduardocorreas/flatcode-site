b4abf95cb2ed593cf33a1b67e568659a50639d19
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2020-04-04 18:45:20 -0300

    use cases components

---
+++ src/components/socialIcons.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+config.autoAddCss = false
+
+export default function SocialIcons() {
+  return (
+    <div className="flex pl-20 pr-20 justify-center">
+      <a
+        href="https://instagram.com/flatcodelab"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon
+          icon={faInstagram}
+          className="text-white w-8  mr-5 ml-5"
+        />
+      </a>
+
+      <a
+        href="https://api.whatsapp.com/send?phone=5591984049539"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon
+          icon={faWhatsapp}
+          className="text-white w-8 mr-5 ml-5"
+        />
+      </a>
+
+      <a
+        href="mailto:flatcode23@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon
+          icon={faEnvelope}
+          className="text-white w-8 mr-5 ml-5"
+        />
+      </a>
+    </div>
+  )
+}
---
+++ src/components/useCases.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import UseCaseItem from './useCaseItem'
+
+import { useStaticQuery, graphql } from 'gatsby'
+export default function UseCases() {
+  const data = useStaticQuery(graphql`
+    query {
+      pojo: file(name: { eq: "pojo_jump" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div className="bg-white w-full p-20">
+      <h2 className="title-section text-gray-800">
+        Casos <span style={{ color: '#ff7400' }}>de uso</span>
+      </h2>
+      <p className="text-gray-800 p-10 text-xl text-center">
+        Os nossos resultados na história de nossos clientes{' '}
+      </p>
+      <div>
+        <UseCaseItem
+          imgNode={data.pojo}
+          caption={'Pojo Personal'}
+          title={
+            'Um salto para a consultoria online: A trajetória de um personal trainer no mundo digital'
+          }
+          description={
+            'Que é fundamental estar nas redes sociais todo mundo sabe! Que é preciso vender todo mundo sabe! Mas e quando você se vê cercado de muitos produtos e profissionais no mesmo ramo? Esse foi o grande desafio do nosso cliente Pedro Pojo: se destacar em um mundo onde existem muitas referências e produtos iguais, mas você precisa se diferenciar!'
+          }
+          link={'/cases/um-salto-para-a-consultoria-online'}
+        />
+      </div>
+    </div>
+  )
+}
