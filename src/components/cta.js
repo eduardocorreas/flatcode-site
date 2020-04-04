0f7e0778e79514da5c9b83138c8e1ffb2f64f66f
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2020-04-04 18:45:00 -0300

    call to action components

---
+++ src/components/cta.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import SocialIcons from '../components/socialIcons'
+
+export default function CTA() {
+  return (
+    <div className="text-center pl-48 pr-48 pb-32">
+      <p className="text-white p-10 text-xl">
+        E aí, como podemos ajudar a sua empresa?{' '}
+      </p>
+      <br />
+      <SocialIcons />
+    </div>
+  )
+}
