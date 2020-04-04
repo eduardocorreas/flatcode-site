9ee7be7d6ae504ca7bad9a9913218b69e4d7e3c4
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2020-04-04 18:42:43 -0300

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Flatcode Lab | Marketing para Pequenos e Médios Empreendedores`,
+    description: `Ajudamos pequenos e médios empreendedores a alavancar sua marca na internet sem precisar perder anos para isso`,
+    author: `@flatcodelab`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,18 +13,42 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/content/cases`,
+        name: `markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `flatcode-site`,
+        short_name: `Flatcode`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2D3748`,
+        theme_color: `#2D3748`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Red Hat Display:300,500,700,900'],
+        },
+      },
+    },
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+        tailwind: true,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
