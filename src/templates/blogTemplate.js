18bc4786e7d68362146cb18842620536d3fa74ce
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2020-04-05 08:34:43 -0300

    remove Header component

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -3,7 +3,6 @@ import { graphql } from 'gatsby'
 
 import SEO from '../components/seo'
 import Layout from '../components/layout'
-import Header from '../components/header'
 import HeroBlog from '../components/heroBlog'
 import CTA from '../components/cta'
 
