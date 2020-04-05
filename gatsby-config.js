a92ab3eed59417df4bca641a339aac57e3eca1be
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2020-04-04 21:30:37 -0300

    Google Analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,12 @@ module.exports = {
         tailwind: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-141375537-1 ',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
