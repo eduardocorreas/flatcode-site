15978d4688c65e3e526d86d1f4fd1cd415bce493
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2020-04-06 11:07:45 -0300

    change to white background

--- src/components/clients.js
+++ src/components/clients.js
@@ -3,8 +3,8 @@ import ClientList from './clientList'
 
 export default function Clients() {
   return (
-    <div className="p-20 mx-auto">
-      <h2 className="title-section text-white mb-10">
+    <div className="p-20 mx-auto  bg-white">
+      <h2 className="title-section mb-10">
         Nossos <span style={{ color: '#ff7400' }}>clientes</span>
       </h2>
       <div className="justify-center mx-auto">
--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,7 @@ import SocialIcons from './socialIcons'
 export default function Contact() {
   return (
     <div className="text-center sm:pl-48 sm:pr-48 pb-20">
-      <h2 className="title-section text-white">Fale conosco</h2>
+      <h2 className="title-section text-white pt-20">Fale conosco</h2>
       <p className="text-white p-10 text-xl">
         Entre em contato conosco e vamos vamos juntos levar a sua empresa para o
         próximo nível!
